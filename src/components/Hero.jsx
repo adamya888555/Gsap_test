861d6ac4527cdd0ac935884af1f48248d94c28e7
Adamya Thakur <adamya8855@Adamyas-MacBook-Air.local>
2026-02-07 02:08:41 +0530

    fixed the glass animation staying in place and video working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,14 @@ import Founder from './Founder'
 import { useGSAP } from '@gsap/react'
 import SplitText from 'gsap/SplitText.js'
 import gsap from 'gsap'
+import { useMediaQuery } from 'react-responsive'
+import { useRef } from 'react'
 
 const Hero = () => {
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
@@ -37,6 +43,24 @@ const Hero = () => {
         })
         .to('.right-leaf', {y: 300}, 0)
         .to('.left-leaf',{y: -200},0)
+
+        const startValue = isMobile?  'top 50%' : 'center 60%';
+        const endValue = isMobile? '120% top': 'bottom top'; 
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
     }, []);
 
 
@@ -44,7 +68,7 @@ const Hero = () => {
     return (
         <>
         <section id='hero' className='noisy'>
-            <h1 className='title'> MOJITO</h1>
+            <h1 className='title'> CAVASQUE</h1>
             <img src='/images/hero-left-leaf.png' alt='left-leaf' className='left-leaf'/>
             <img src='/images/hero-right-leaf.png' alt='right-leaf' className='right-leaf'/>
 
@@ -66,6 +90,15 @@ const Hero = () => {
             </div>
 
         </section>
+        <div className='video absolute inset-0'>
+            <video 
+            ref={videoRef}
+            src='/videos/output.mp4'
+            muted
+            playsInline
+            preload='auto'
+            />
+        </div>
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             <div>
                 <a href="#home" className={"flex items-center gap-2"}>
                     <img src="images/logo.png" alt='logo'></img>
-                    <p>Velvet pour</p>
+                    <p>Cavasque</p>
                 </a>
                 <ul>
                     {navLinks.map((link) => (
